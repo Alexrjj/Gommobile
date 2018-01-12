090fd99a63a963e6b2d09a9e0ecb19fafa41a66c
Alex Piter <alexsoares2517@gmail.com>
2018-01-12 13:34:11 -0200

    removidos códigos e comentários desnecessários

--- Gommobile.py
+++ Gommobile.py
@@ -30,7 +30,7 @@ driver = webdriver.Chrome()
 
 if __name__ == '__main__':
     driver.get(url)
-    # Faz login no sistema
+    # Faz login no sistema GOMNET
     uname = driver.find_element_by_name('txtBoxLogin')
     uname.send_keys(username)
     passw = driver.find_element_by_name('txtBoxSenha')
@@ -38,50 +38,49 @@ if __name__ == '__main__':
     submit_button = driver.find_element_by_id('ImageButton_Login').click()
     driver.get(gommobile)
 
+    # Faz login no sistema GOMMOBILE
     mobLogin = driver.find_element_by_id('ctl00_ContentPlaceHolder1_txtLogin')
     mobLogin.send_keys(mobUser)
     mobSenha = driver.find_element_by_id('ctl00_ContentPlaceHolder1_txtSenha')
     mobSenha.send_keys(mobPass)
     entrarBtn = driver.find_element_by_id('ctl00_ContentPlaceHolder1_btnEnvia').click()
 
-    # syncBtn = driver.find_element_by_id('ctl00_ContentPlaceHolder1_lbSincronizar').click()
-    # time.sleep(5)
-    syncBtn = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolder1_lbSincronizar')))
-    syncBtn.click()
-    # okBtn = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/button/span').click()
-    okBtn = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[3]/div/button/span')))
-    okBtn.click()
-    # tarefaBtn = driver.find_element_by_id('ctl00_ContentPlaceHolder1_lbTarefaPendente').click()
-    tarefaBtn = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolder1_lbTarefaPendente')))
-    tarefaBtn.click()
-    # time.sleep(10)
+    # Sincroniza as tarefas
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolder1_lbSincronizar'))).click()
+    okBtn = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[3]/div/button/span'))).click()
+    # Clica nas tarefas pendentes
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolder1_lbTarefaPendente'))).click()
 
+    # Acessa os dados na planilha 'sobs.xlsx' para começar a trabalhar.
     for sheet in wb.worksheets:
-#        try: # Procura os baremos na planilha "sobs.xlsx" e marca de acordo
-            # driver.find_element_by_xpath("//*[contains(text(), '" + str(sheet['A1'].value) + "')]").click()
-
-            obra = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "*//tr/td[contains(text(), '" + str(sheet['A1'].value) + "')]")))
-            obra.click()
-            # driver.find_element_by_xpath("*//tr/td[contains(text(), '" + str(sheet['A1'].value) + "')]").click()
-            #descSob = driver.find_element_by_xpath("*//tbody/tr/td/[contains(text(), 'A019719314')]")
-            #webdriver.ActionChains(driver).click(descSob).perform()
-            #driver.find_element_by_xpath("//*[@id='gvRelatorio']/tbody/tr/td[contains(text(), '" + str(sheet['A1'].value) + "')]").click()
+        # try:
+            # Busca o valor da SOB e clica
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "*//tr/td[contains(text(), '" + str(sheet['A1'].value) + "')]"))).click()
+            # Pressina o TAB uma vez e depois ENTER, para abrir a janela de inserção de dados para a SOB.
             webdriver.ActionChains(driver).send_keys(Keys.TAB).perform()
             webdriver.ActionChains(driver).send_keys(Keys.RETURN).perform()
-
+            # Seleciona todos os baremos executados
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gvItens"]/tbody/tr[1]/th[1]'))).click()
+            # Clica na aba "Geral"
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="tabGeral"]'))).click()
+            # Clica na caixa de informação de "Saída"
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="txtDtSaida"]'))).click()
+            # Clica no botão "agora"
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ui-datepicker-div"]/div[3]/button[1]'))).click()
+            # Clica na caixa de informação de "Início Tarefa"
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="txtIniTarefa"]'))).click()
+            # Clica no botão "agora"
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ui-datepicker-div"]/div[3]/button[1]'))).click()
-            # WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[3]/div/button[1]/span'))).click() # Finaliza a SOB
-            # driver.find_element_by_xpath("*//tr/td[contains(text(), '" + str(cell.value) + "')]/preceding-sibling::td/input").click()
-#        except NoSuchElementException:  # Caso não encontre, abre o arquivo txt e registra a data, o código baremo e sua quantidade
-        #    log = open("BaremosPendentes.txt", "a")
-        #    log.write(str(sheet['H1'].value) + " " + str(sheet['A1'].value) + " " + str(cell.value) + " " + str(cell2.value) + "\n")
-        #    log.close()
-#           continue
-#  Ao fim do loop de inserção de baremos, clica no botão "registrar programação"
+            
+            # Finaliza a SOB
+            # WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[3]/div/button[1]/span'))).click()
+            # Cancela a SOB
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[3]/div/button[2]/span'))).click()
+        # except NoSuchElementException:  # Caso não encontre, abre o arquivo txt e registra a data, o código baremo e sua quantidade
+            # log = open("BaremosPendentes.txt", "a")
+            # log.write(str(sheet['H1'].value) + " " + str(sheet['A1'].value) + " " + str(cell.value) + " " + str(cell2.value) + "\n")
+            # log.close()
+           # continue
+# Ao fim do loop de inserção de baremos, clica no botão "registrar programação"
 #driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_btnEnviarItens"]').click()
 #print(str(sheet['A1'].value) + " programada com êxito.")
